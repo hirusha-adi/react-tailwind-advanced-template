9fcf67e15f57b8ddc432adaf6bca04f9bc00dd6e
Hirusha Adikari <hirushaadi@gmail.com>
2024-11-18 22:13:40 -0500

    use common layout + route in middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
+import { AllRoutes } from "./routes/AllRoutes"
+import { Header, Footer } from './components'
 
 function App() {
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<AllRoutes />
+			<Footer />
+		</>
+	)
 }
 
 export default App
